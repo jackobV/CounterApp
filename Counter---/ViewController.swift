64a92a320a92e222c79c5ff0303d3423f04464bf
jacob <kuba.zaloha@icloud.com>
2019-12-28 17:09:05 +0100

    Initial Commit

---
+++ Counter---/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Counter---
+//
+//  Created by Jakub Záloha on 28/12/2019.
+//  Copyright © 2019 Jakub Záloha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
